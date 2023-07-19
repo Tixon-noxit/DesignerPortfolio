13487e0c0793c22da3513fbd59d89dffcdd1c13e
Chabusov Tikhon <chabysow@gmail.com>
2023-07-20 01:13:45 +0300

    develop add deleted files

--- main/static/main/js/active_page.js
+++
@@ -1,9 +0,0 @@
- $(document).ready(function () {
-     $('.site-menu a').each(function () {
-         let location = window.location.protocol + '//' + window.location.host + window.location.pathname;
-         let link = this.href;
-         if(location === link) {
-             $(this).parent().addClass('active')
-         }
-     })
- })
